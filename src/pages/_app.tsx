810e701b1dabee067b2076b14a245f07c1097d01
chandresh patidar <patidarchandresh2002@gmail.com>
2023-04-23 18:01:29 +0530

    feat: add vercel analytics to site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+
 import PageLayout from 'layout/PageLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -8,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <PageLayout>
         <Component {...pageProps} />
       </PageLayout>
+      <Analytics />
     </>
   );
 }
